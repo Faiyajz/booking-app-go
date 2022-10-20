e7e3b578efab375b4e1e4c751b4b0a2a9884ce9a
faiyaj <faiyajz007@gmail.com>
2022-10-20 14:19:05 +0600

    take input from user

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	const conferenceTickets = 50
 	// const conferenceTickets int = 50
 
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
 
@@ -19,12 +19,28 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	userName = "Faiyaj"
-	userTickets = 2
+	// fmt.Println(remainingTickets)
+	// fmt.Println(&remainingTickets) pointer is referring the memory address using a hash(Pointer)
 
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	fmt.Println("Please enter your first name: ")
+	fmt.Scan(&firstName)
 
+	fmt.Println("Please enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets you want to book: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }

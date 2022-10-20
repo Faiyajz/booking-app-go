153a02665346fcb13cf013708860d77a301cf46d
faiyaj <faiyajz007@gmail.com>
2022-10-20 11:55:32 +0600

    variable declaration in different way using colon and equal

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	// var conferenceName string = "Go Conference"
+
+	conferenceName := "Go Conference" //we can't declare const variable with this alternative syntax
+
 	const conferenceTickets = 50
+	// const conferenceTickets int = 50
+
 	var remainingTickets = 50
+
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
-	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 

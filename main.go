117023a679c1b1cc305524f7e7b256761f0767ee
faiyaj <faiyajz007@gmail.com>
2022-10-20 18:44:17 +0600

    For Loop

--- main.go
+++ main.go
@@ -24,36 +24,38 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	// fmt.Println(remainingTickets)
-	// fmt.Println(&remainingTickets) pointer is referring the memory address using a hash(Pointer)
+		// fmt.Println(remainingTickets)
+		// fmt.Println(&remainingTickets) pointer is referring the memory address using a hash(Pointer)
 
-	fmt.Println("Please enter your first name: ")
-	fmt.Scan(&firstName)
+		fmt.Println("Please enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Please enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Please enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Please enter your email: ")
-	fmt.Scan(&email)
+		fmt.Println("Please enter your email: ")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter number of tickets you want to book: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter number of tickets you want to book: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("The whole slice: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Slice type: %T\n", bookings)
-	fmt.Printf("Slice Size: %v\n", len(bookings))
+		fmt.Printf("The whole slice: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("Slice type: %T\n", bookings)
+		fmt.Printf("Slice Size: %v\n", len(bookings))
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("These are all our bookings: %v\n",bookings)
+		fmt.Printf("These are all our bookings: %v\n",bookings)
+	}
 }

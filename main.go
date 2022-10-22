6cb232b0bc15c13e6b98842ee7eede3b57ac4c46
faiyaj <faiyajz007@gmail.com>
2022-10-22 22:02:46 +0600

    function optimization moving variables outside the main function and remove function parameter for globar variables

--- main.go
+++ main.go
@@ -5,45 +5,29 @@ import (
 	"strings"
 )
 
+var conferenceName = "Go Conference"
+
+const conferenceTickets = 50
+
+var remainingTickets uint = 50
+var bookings = []string{}
+
 func main() {
-	// var conferenceName string = "Go Conference"
 
-	conferenceName := "Go Conference" //we can't declare const variable with this alternative syntax
+	greetUsers()
 
-	const conferenceTickets = 50
-	// const conferenceTickets int = 50
+	for {
 
-	var remainingTickets uint = 50
-	var bookings []string //Slice Declaration
-	// var bookings = []string{} //alternative way of declaring slice
-	// bookings := []string{} // Another alternative way of declaring slice
-
-	greetUsers(conferenceName, conferenceTickets, remainingTickets)
-
-	for remainingTickets > 0 && len(bookings) < 50 {
-
-		//Call function to take user inputs
 		firstName, lastName, email, userTickets := getUserInput()
 
-		//Call function to validate user inputs
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
-
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
 
-			remainingTickets = remainingTickets - userTickets
-			bookings = append(bookings, firstName+" "+lastName)
-
-			fmt.Printf("The whole slice: %v\n", bookings)
-			fmt.Printf("The first value: %v\n", bookings[0])
-			fmt.Printf("Slice type: %T\n", bookings)
-			fmt.Printf("Slice Size: %v\n", len(bookings))
-
-			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+			bookTicket(firstName, lastName, userTickets, email)
 
 			//Call function to print the first names
-			firstNames := getFirstNames(bookings)
+			firstNames := getFirstNames()
 			fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
 			if remainingTickets == 0 {
@@ -79,18 +63,16 @@ func main() {
 
 }
 
-func greetUsers(confName string, confTickets int, remainTickets uint) {
+func greetUsers() {
 
-	fmt.Printf("Welcome to %v booking application\n", confName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n", confTickets, remainTickets)
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
 
-//Passing parameter section inside the brackets is input parameter and outside the
-//brackets is the output parameter
+func getFirstNames() []string {
 
-func getFirstNames(bookings []string) []string {
-	firstNames := []string{} //another way of declaring-- var firstNames []string
+	firstNames := []string{}
 
 	for _, booking := range bookings {
 		var names = strings.Fields(booking)
@@ -101,8 +83,8 @@ func getFirstNames(bookings []string) []string {
 
 }
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
-	
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
 	isValidEmail := strings.Contains(email, "@")
 	isValidTicketNumber := userTickets > 0 && userTickets < remainingTickets
@@ -110,15 +92,12 @@ func validateUserInput(firstName string, lastName string, email string, userTick
 	return isValidName, isValidEmail, isValidTicketNumber
 }
 
-func getUserInput() (string, string, string, uint){
+func getUserInput() (string, string, string, uint) {
 
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
-
-	// fmt.Println(remainingTickets)
-	// fmt.Println(&remainingTickets) pointer is referring the memory address using a hash(Pointer)
 
 	fmt.Println("Please enter your first name: ")
 	fmt.Scan(&firstName)
@@ -133,4 +112,14 @@ func getUserInput() (string, string, string, uint){
 	fmt.Scan(&userTickets)
 
 	return firstName, lastName, email, userTickets
+}
+
+func bookTicket(firstName string, lastName string, userTickets uint, email string) {
+
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
 }
